71a3cde0416b270437a005fbb17d82f9aa70e251
Ringo Hoffmann <contact@zekro.de>
2022-01-20 11:26:11 +0100

    add paged request

--- stargrab/github.py
+++ stargrab/github.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import requests
 
 
@@ -8,9 +8,10 @@ _GH_GQL_ENDPOINT = "https://api.github.com/graphql"
 
 class Repository:
     def __init__(self, v: Dict):
-        self.owner_login = v.get('owner').get('login')
-        self.name = v.get('name')
-        self.url = v.get('url')
+        self.cursor = v.get("cursor")
+        self.owner_login = v.get("node").get('owner').get('login')
+        self.name = v.get("node").get('name')
+        self.url = v.get("node").get('url')
 
 
 class Client:
@@ -21,12 +22,29 @@ class Client:
 
     def get_starred_repositories(self,
                                  user_override=None) -> List[Repository]:
+        repos = []
+        after = ""
+        while True:
+            (total, r) = self._get_starred_repositories_paged(
+                user_override, after)
+            repos += r
+            if len(repos) >= total:
+                break
+            after = r[-1].cursor
+        return repos
+
+    def _get_starred_repositories_paged(self,
+                                        user_override: str,
+                                        after: str
+                                        ) -> Tuple[int, List[Repository]]:
         user = f'user(login:"{user_override}")' if user_override else "viewer"
         query = f"""
             query {{
                 {user} {{
-                    starredRepositories {{
+                    starredRepositories(after:"{after}") {{
+                        totalCount
                         edges {{
+                            cursor
                             node {{
                                 owner {{
                                     login
@@ -40,11 +58,12 @@ class Client:
             }}
         """
         res = self._request(query)
-        repos = res.get("data") \
-                   .get("user" if user_override else "viewer") \
-                   .get("starredRepositories") \
-                   .get("edges")
-        return [Repository(r.get("node")) for r in repos]
+        starred = res.get("data") \
+                     .get("user" if user_override else "viewer") \
+                     .get("starredRepositories")
+        total = starred.get("totalCount")
+        repos = starred.get("edges")
+        return (total, [Repository(r) for r in repos])
 
     def _request(self, query: str):
         data = json.dumps({
